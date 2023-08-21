ce29ef00af099e0496279d9064f72aa66e972691
laraib <labbuxt@gmail.com>
2023-08-22 00:24:49 +0530

    hows that?

--- laraib_git/health/lib/main.dart
+++ laraib_git/health/lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Healthify',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'LABROX',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.white)),
+      home: const Home(),
     );
   }
 }
-
-class _MyHomePageState extends State<MyHomePage> {}
