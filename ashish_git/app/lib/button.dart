8423ecc807acac76d5130553e787eea78e30fef3
ashish kumar singh <officialwork.ashish@gmail.com>
2023-08-22 23:48:29 +0530

    BUTTON ADDED

---
+++ ashish_git/app/lib/button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'loginpage.dart';
+
+
+final ButtonStyle buttonPrimary = ElevatedButton.styleFrom(
+  minimumSize: Size( 327 , 40),
+  backgroundColor: Color.fromARGB(255, 140, 31, 214),
+  elevation : 0,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular( 10 ),
+  ),
+
+);
