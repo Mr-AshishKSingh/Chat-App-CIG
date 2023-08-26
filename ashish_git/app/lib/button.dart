1a46ac46446942fb7822610ec898016e03fdcdd8
ashish kumar singh <officialwork.ashish@gmail.com>
2023-08-26 16:50:09 +0530

    2WELCOME PAGES ADDED

--- ashish_git/app/lib/button.dart
+++ ashish_git/app/lib/button.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
-import 'loginpage.dart';
+import 'welcome.dart';
 
 
 final ButtonStyle buttonPrimary = ElevatedButton.styleFrom(
   minimumSize: Size( 327 , 40),
-  backgroundColor: Color.fromARGB(255, 140, 31, 214),
+  backgroundColor: Color.fromARGB(255, 61, 35, 207),
   elevation : 0,
   shape: RoundedRectangleBorder(
     borderRadius: BorderRadius.circular( 10 ),
+  ),
+
+);
+
+
+
+final ButtonStyle button2Primary = ElevatedButton.styleFrom(
+  minimumSize: Size( 315 , 40),
+  backgroundColor: Color.fromARGB(255, 61, 35, 207),
+  elevation : 0,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular( 100 ),
+  ),
+
+);
+
+final ButtonStyle gobackbutton = ElevatedButton.styleFrom(
+  minimumSize: Size( 315 , 40),
+  backgroundColor: Color.fromARGB(255, 50, 60, 254),
+  elevation : 0,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular( 100 ),
   ),
 
 );
