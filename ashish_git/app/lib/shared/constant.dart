ad907a655148f8d09bccb1ee17fbd958d6d30605
Ashish Singh <110713754+Mr-AshishKSingh@users.noreply.github.com>
2023-12-25 14:29:35 +0530

    Delete ashish_git/app directory

--- ashish_git/app/lib/shared/constant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-  static String appId = "1:1024944649822:web:0fb018170b1086cc5a66cf";
-  static String apiKey = "AIzaSyBRuazVge4WWfCl3UfehAgOfnIttuRjC84";
-  static String messagingSenderId = "1024944649822";
-  static String projectId = "heathapp-50101";
-  final primaryColor = const Color(0xFFee7b64);
-}
